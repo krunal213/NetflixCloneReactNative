c9113788f7addb0f4306b2f4e6d1a20b301a553d
krunal21041993 <kathikar.krunal3@gmail.com>
2026-03-29 19:12:09 +0530

    Splash Screen Implmentation

---
+++ src/container.tsx
@@ -0,0 +1,24 @@
+import { container } from "tsyringe";
+
+/*container.register("AxiosInstance", {
+  useValue: api,
+});
+
+container.register("AsyncStorage", {
+  useValue: AsyncStorage,
+});
+
+container.register<NetworkDataSource>("NetworkDataSource", {
+  useClass: NetworkDataSourceImpl,
+});
+
+container.register<LocalDataSource>("LocalDataSource", {
+  useClass: LocalDataSourceImpl,
+});
+
+container.register<Repository>("Repository", {
+  useClass: RepositoryImpl,
+});*/
+
+
+
--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import homeReducer from './view/home/homeSlice'
+import splashReducer from './view/splash/splashSlice'
 
 export const store = configureStore({
     reducer : {
+        splash : splashReducer,
         home : homeReducer
     }
 })
